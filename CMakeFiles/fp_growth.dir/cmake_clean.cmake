4f07d1fa02812534f99ccc6daa661fb789c4493c
yarnaid <yarnaid@gmail.com>
2015-03-24 20:18:54 +0300

    changes...

---
+++ CMakeFiles/fp_growth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fp_growth.dir/fptree.cpp.o"
+  "libfp_growth.pdb"
+  "libfp_growth.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fp_growth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- fptree.hpp
+++ fptree.hpp
@@ -7,9 +7,35 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <iostream>
+#include <ctime>
 
 
-using Item = std::string;
+struct Item {
+    time_t time;
+    std::string id;
+    std::string source;
+    std::string category;
+    std::string name;
+    std::string md5;
+    std::string ip;
+    std::string url;
+    std::string fqdn;
+    std::string asn;
+    std::string cc;
+    std::string details;
+
+    int svm_class;
+    std::string token;
+
+    bool operator==(const Item& other) const {
+        return (token == other.token);
+    }
+
+    bool operator<(const Item& other) const {
+        return token < other.token;
+    }
+};
 using Transaction = std::vector<Item>;
 using TransformedPrefixPath = std::pair<std::vector<Item>, unsigned>;
 using Pattern = std::pair<std::set<Item>, unsigned>;
@@ -21,7 +47,11 @@ struct FPNode {
     std::shared_ptr<FPNode> node_link;
     std::shared_ptr<FPNode> parent;
     std::vector<std::shared_ptr<FPNode>> children;
-    
+    std::string token;
+    int depth;
+
+    int label;
+
     FPNode(const Item&, const std::shared_ptr<FPNode>&);
 };
 
@@ -31,14 +61,16 @@ struct FPTree {
     std::shared_ptr<FPNode> root;
     std::map<Item, std::shared_ptr<FPNode>> header_table;
     unsigned minimum_support_treshold;
-    
+
     FPTree(const std::vector<Transaction>&, unsigned);
-    
+
     bool empty() const;
 };
 
 
 std::set<Pattern> fptree_growth(const FPTree&);
+void print_pattern(const std::set<Pattern>&);
+std::ostream& operator<<(std::ostream&, const Item&);
 
 
 #endif  // FPTREE_HPP
